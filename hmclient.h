fdb00e7f3f1f4bb6b94f22a6e5cceecb909e7bc0
IceMan66 <IceMan66@users.noreply.github.com>
2016-11-10 12:52:17 +0100

    Update hmclient.h

--- hmclient.h
+++ hmclient.h
@@ -3,6 +3,19 @@
 #ifndef HMCLIENT_H
 #define HMCLIENT_H
 
+class HMClient{
+  public:
+    /* constructor */
+    HMClient();
+  
+    /* read function */
+    boolean getHMSystemVariable();
+  
+    /* write function */
+    boolean setHMSystemVariable();
+  private:
+  
+}
 
 
 #endif #HMCLIENT_H
