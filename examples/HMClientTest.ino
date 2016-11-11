4fda900b84b94417a9833a2069656f50d97f5dac
IceMan66 <IceMan66@users.noreply.github.com>
2016-11-11 11:13:31 +0100

    Update HMClientTest.ino

--- examples/HMClientTest.ino
+++ examples/HMClientTest.ino
@@ -1,1 +1,18 @@
+
+#include <ESP8266WiFi.h>
 #include <HMClient.h>
+
+const char* ssid     = "Your SSID";
+const char* passcode = "Your PASSCODE";
+
+HMClient HM_Default;
+HMClient HMU_UserDefined("Server1");
+
+void setup() {
+  // put your setup code here, to run once:  Serial.begin(115200);  Serial.flush();  Serial.println("Booting");    WiFi.begin(ssid, password);    while (WiFi.status() != WL_CONNECTED) {    delay(500);    Serial.print(".");  }  Serial.println("Connected");
+}
+
+void loop() {
+
+  
+}
