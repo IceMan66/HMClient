e5d498f51eaa780b1ba942e0a803474ecb4d5b7b
IceMan66 <IceMan66@users.noreply.github.com>
2016-11-11 10:57:32 +0100

    Update hmclient.cpp

--- hmclient.cpp
+++ hmclient.cpp
@@ -3,8 +3,12 @@
  */
  #include "hmclient.h"
  
+//
+//  Construct a new HMClient object using the default CCU2 address.
+//
  HMClient::HMClient(){
-	 
+	m_host = "homematic-ccu2";
+	m_port = 8181; 
  }
  
  boolean HMClient::setHMSystemVariable(){
